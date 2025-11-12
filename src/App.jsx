f37ca69d54f295543f71a6ecabc8ce201007fedc
Sachin Zanzrukiya <zsachin219@gmail.com>
2025-11-12 09:31:44 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -236,7 +236,7 @@ function App() {
           </div>
         </div>
       </section>
-      
+
       <section id="projects">
         <div className="container projects-container">
           <h2 className="section-title">My Projects</h2>
@@ -259,12 +259,6 @@ function App() {
                 >
                   View Code
                 </a>
-                <a
-                  href="https://construction-blush.vercel.app/"
-                  target="_blank"
-                >
-                  Live Demo
-                </a>
               </div>
             </div>
             <div className="project-card">
@@ -280,12 +274,6 @@ function App() {
                   target="_blank"
                 >
                   View Code
-                </a>
-                <a
-                  href="https://construction-blush.vercel.app/"
-                  target="_blank"
-                >
-                  Live Demo
                 </a>
               </div>
             </div>
@@ -320,13 +308,181 @@ function App() {
                   React, Node, SQL, and Responsive.
                 </p>
                 <a
-                  href="https://github.com/yourusername/todo-app"
+                  href="https://github.com/SACHIN6612/restaurant-website"
                   target="_blank"
                 >
                   View Code
                 </a>
                 <a
-                  href="https://construction-blush.vercel.app/"
+                  href="https://restaurant-website-opal-six.vercel.app/"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img src="images/project-5.png" alt="Project 5 Screenshot" />
+              <div className="project-content">
+                <h3>Icecream Parlour Website</h3>
+                <p className="text">
+                  A modern and responsive Ice Cream Parlour website built using
+                  HTML, CSS, JavaScript, and React.js, featuring product
+                  showcase, attractive UI, offers section, and contact form.
+                </p>
+                <a
+                  href="https://github.com/SACHIN6612/ice-cream-parlour"
+                  target="_blank"
+                >
+                  View Code
+                </a>
+                <a
+                  href="https://ice-cream-parlour-virid.vercel.app/"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img src="images/project-6.png" alt="Project 6 Screenshot" />
+              <div className="project-content">
+                <h3>Portfolio Website</h3>
+                <p className="text">
+                  The Portfolio Website is a personal web project built to
+                  showcase my skills, projects, and achievements as a Full Stack
+                  Developer.
+                </p>
+                <a
+                  href="https://github.com/SACHIN6612/Portfolio"
+                  target="_blank"
+                >
+                  View Code
+                </a>
+                <a
+                  href="https://portfolio-phi-kohl-80.vercel.app/"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img src="images/project-7.png" alt="Project 7 Screenshot" />
+              <div className="project-content">
+                <h3>Job Portal Website</h3>
+                <p className="text">
+                  Designed and implemented the full frontend using React
+                  components, reusable CSS modules, and Bootstrap Framework.
+                </p>
+                <a
+                  href="https://github.com/SACHIN6612/Job-Portal"
+                  target="_blank"
+                >
+                  View Code
+                </a>
+                <a
+                  href="https://job-portal-three-sepia.vercel.app/"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img src="images/project-8.png" alt="Project 8 Screenshot" />
+              <div className="project-content">
+                <h3>Calculator Web App</h3>
+                <p className="text">
+                  A modern, responsive calculator application built using HTML5,
+                  CSS3, and JavaScript (ES6+). Designed with clean UI and
+                  supports arithmetic operations including addition,
+                  subtraction, multiplication and division.
+                </p>
+                <a
+                  href="https://github.com/SACHIN6612/Calculator"
+                  target="_blank"
+                >
+                  View Code
+                </a>
+                <a
+                  href="https://calculator-orpin-eta-15.vercel.app/"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img src="images/project-9.png" alt="Project 9 Screenshot" />
+              <div className="project-content">
+                <h3>Counter App</h3>
+                <p className="text">
+                  The CounterApp is a web-based frontend project that allows
+                  users to increment, decrement, and reset a numeric value
+                  through a clean and responsive interface.
+                </p>
+                <a
+                  href="https://github.com/SACHIN6612/CounterApp"
+                  target="_blank"
+                >
+                  View Code
+                </a>
+                <a
+                  href="https://counter-app-lac-phi.vercel.app/"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img src="images/project-10.png" alt="Project 10 Screenshot" />
+              <div className="project-content">
+                <h3>To Do App</h3>
+                <p className="text">
+                  A clean, responsive To-Do Application built using HTML5, CSS3
+                  (global CSS), JavaScript (ES6+), and React Js, allowing users
+                  to add tasks as complete.
+                </p>
+                <a
+                  href="https://github.com/SACHIN6612/To-Do-App"
+                  target="_blank"
+                >
+                  View Code
+                </a>
+                <a
+                  href="https://to-do-app-iota-amber.vercel.app/"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img src="images/project-11.png" alt="Project 11 Screenshot" />
+              <div className="project-content">
+                <h3>Business Name Generator</h3>
+                <p className="text">
+                  A sleek and responsive web application that generates creative
+                  and brand-worthy business names based on user input keywords.
+                  Built using HTML5, CSS3 (global CSS), JavaScript (ES6+).
+                </p>
+                <a
+                  href="https://github.com/SACHIN6612/Business-Name-Generator"
+                  target="_blank"
+                >
+                  View Code
+                </a>
+                <a
+                  href="https://business-name-generator-six.vercel.app/"
                   target="_blank"
                 >
                   Live Demo
